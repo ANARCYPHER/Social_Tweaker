981d3a719686d935359260d79b1f79aa1ea233e5
AllSafeCybercurity <skull32273227@gmail.com>
2023-03-18 06:45:56 -0700

    p02

--- social_tweaker/tweaker/urls.py
+++ social_tweaker/tweaker/urls.py
@@ -1,10 +1,15 @@
 """ₐₚₚ_ₜwₑₐₖₑᵣ ᵤᵣₗ 𝄴ₒₙ𝆑ᵢgᵤᵣₐₜᵢₒₙ
 """
-
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name="index"),
-  
+    path('profile_list/', views.profile_list, name='profile_list'),
+    path('profile/<int:pk>', views.profile, name='profile'),
+    path('login/', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('update_user/', views.update_user, name='update_user'),
 ]
